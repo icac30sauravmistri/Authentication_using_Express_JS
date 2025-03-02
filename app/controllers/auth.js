18b06262bc275776a46e826dcc41bb84cb3ff14b
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-03 00:32:26 +0530

    login logic code is done

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 /**
  * @route POST v1/auth/register
@@ -50,4 +51,51 @@ export async function Register(req, res) {
         });
     }
     res.end()
+}
+
+export async function Login(req, res) {
+    // Get variables for the login process
+    const { email } = req.body;
+
+    try {
+        // Check if user exists
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(401).json({
+                status: "failed",
+                data: [],
+                message: "Invalid email or password. Please try again with the correct/valid credentials.",
+            });
+        }
+        // if user exists 
+        // validate password
+        const isPasswordValid = await bcrypt.password(
+            `${req.body.password}`,
+            user.password
+        );
+        // if not valid, return unauthorized response 
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                status: "failed",
+                data: [],
+                message: "Invalid email or password. Please try again with the correct/valid credentials.",
+            });
+        }
+        // return user info except password
+        const { password, ...user_data } = user._doc;
+
+        res.status(200).json({
+            status: "success",
+            data: [user_data],
+            message: "You have successfully logged-in!",
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: "Internal Server Error!",
+        });
+    }
+    res.end();
 }
