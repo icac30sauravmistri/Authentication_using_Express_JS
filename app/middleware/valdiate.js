79b6adc526c41c2732c483c8456dda8c566a465c
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-02 23:01:56 +0530

    input validation code is done

---
+++ app/middleware/valdiate.js
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+
+const validate = (req, res, next) => {
+    const errors = validationResult();
+    if (!errors.isEmpty()) {
+        let error = {};
+        errors.array().map((err) => (error[err.param] = err.msg));
+        return res.status(422).json({ error });
+    }
+    next();
+};
+
+export default validate;
