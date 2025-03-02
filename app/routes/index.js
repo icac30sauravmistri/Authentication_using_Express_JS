4c8cdb66631bb800269fdfef617d2c7e08e427b0
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-03 02:31:39 +0530

    resolved error

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
+import { Verify } from "../middleware/verify.js";
+
 const Router = (app) => {
     // home route with the get method and a handler
-    app.get("/app", (req, res) => {        
+    app.get("/app", (req, res) => {
         try {
             res.status(200).json({
                 status: "success",
@@ -13,6 +15,14 @@ const Router = (app) => {
                 message: "Internal Server Error",
             });
         }
-    })
+    });
+
+    // session verify route
+    app.get("/app/user", Verify, (req, res) => {
+        res.status(200).json({
+            status: "success",
+            message: "Welcome to your Dashboard!",
+        });
+    });
 };
 export default Router;
