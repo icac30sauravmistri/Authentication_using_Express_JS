8630b2cdb1e28f28a5af3eccd24ffe1c9fd4bea8
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-03 02:42:10 +0530

    session token will be blacklisted

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import Blacklist from "../models/Blacklist.js";
 
 /**
  * @route POST v1/auth/register
@@ -84,15 +85,15 @@ export async function Login(req, res) {
 
         let options = {
             // generated token would be expired in 10 min
-            maxAge:10*60*1000,
+            maxAge: 10 * 60 * 1000,
             // The cookie is only accessible by the web server
-            httpOnly:true,
+            httpOnly: true,
             secure: true,
-            sameSite:"None",
+            sameSite: "None",
         };
 
         // generate session token for user
-        const token=user.generateAccessJWT();
+        const token = user.generateAccessJWT();
 
         // set the token to response header, so that the client sends it back on each subsequent request
         res.cookie("SessionID", token, options);
@@ -111,6 +112,37 @@ export async function Login(req, res) {
             code: 500,
             data: [],
             message: "Internal Server Error!",
+        });
+    }
+    res.end();
+}
+
+/**
+ * @route POST /auth/logout
+ * @desc Logout user
+ * @access Public
+ */
+export async function Logout(req, res) {
+    try {
+        const authHeader = req.headers['cookie']; // get the session cookie from request header
+        if (!authHeader) return res.sendStatus(204); // No content
+        const cookie = authHeader.split('=')[1]; // If there is, split the cookie string to get the actual jwt token
+        const accessToken = cookie.split(';')[0];
+        const checkIfBlacklisted = await Blacklist.findOne({ token: accessToken }); // Check if that token is blacklisted
+        // if true, send a no content response.
+        if (checkIfBlacklisted) return res.sendStatus(204);
+        // otherwise blacklist token
+        const newBlacklist = new Blacklist({
+            token: accessToken,
+        });
+        await newBlacklist.save();
+        // Also clear request cookie on client
+        res.setHeader('Clear-Site-Data', '"cookies"');
+        res.status(200).json({ message: 'You are logged out!' });
+    } catch (err) {
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error',
         });
     }
     res.end();
