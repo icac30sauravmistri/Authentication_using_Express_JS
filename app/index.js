c68e36cc87a1e35441f4c92d0120643ad8babbc4
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-02 22:34:28 +0530

    necessary package and mongodb connection is done

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 import { PORT, URI } from "./config/index.js";
 import Router from "./routes/index.js";
 
@@ -7,6 +10,27 @@ import Router from "./routes/index.js";
 // Create Server
 const app = express();
 
+// step-1: Configure Header Information
+// Allow request from any source. In real production, this should be limited to allowed origins only
+app.use(cors());
+app.disable("x-powered-by"); //Reduce fingerprinting
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// step 2: Connect Database
+// Set up mongoose's promise to global promise
+mongoose.promise = global.Promise;
+mongoose.set("strictQuery", false);
+mongoose
+    .connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log("Connected to database"))
+    .catch((err) => console.log(err));
+
+// step4: Configure Routes ===
 // Connect route handler to app
 Router(app);
 
